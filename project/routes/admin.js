18cad2b151a6868948ff913b90be1e4b1d574ecf
liadse <58271429+liadse@users.noreply.github.com>
2021-05-29 20:14:16 +0300

    add the admin (nihull liga done)
    /

--- project/routes/admin.js
+++ project/routes/admin.js
@@ -46,8 +46,8 @@ router.post("/addPreviewMatch", async (req, res, next) => {
         }
         else {
             await DButils.execQuery(
-                `INSERT INTO dbo.Matches (Date, Hour, Stadium, SuperligaName, SeasonName, StageName, HomeTeamID, AwayTeamID, RefereeName) VALUES 
-                ('${req.body.previewMatch.date}','${req.body.previewMatch.hour}', '${req.body.previewMatch.stadium}','${req.body.previewMatch.superligaName}','${req.body.previewMatch.seasonName}','${req.body.previewMatch.stageName}','${req.body.previewMatch.homeTeamID}','${req.body.previewMatch.awayTeamID}')`
+                `INSERT INTO dbo.Matches (Date, Hour, Stadium, SuperligaName, SeasonName, StageName, HomeTeamID, AwayTeamID) VALUES 
+                ('${req.body.date}','${req.body.hour}', '${req.body.stadium}','${req.body.superligaName}','${req.body.seasonName}','${req.body.stageName}','${req.body.homeTeamID}','${req.body.awayTeamID}')`
                 );
             res.status(201).send("match that was not played was added succesfully");
         }
@@ -68,7 +68,7 @@ router.put("/addScoretoMatch", async (req, res, next) => {
             const AwayGoals = req.body.AwayGoals;
 
             await DButils.execQuery(
-                `update dbo.Matches set HomeGoals = '${HomeGoals}' and AwayGoals = '${AwayGoals}' where MatchID = '${MatchId}'`
+                `update dbo.Matches set HomeGoals = '${HomeGoals}' , AwayGoals = '${AwayGoals}' where MatchID = '${MatchId}'`
             );
             res.status(201).send("score has been added to match");
         }
@@ -80,12 +80,12 @@ router.put("/addScoretoMatch", async (req, res, next) => {
 router.post("/addEventtoMatch", async (req, res, next) => {
     try {
         const user_id = req.session.user_id;
-        if (user_id != 1) {
+        if (user_id != 3) {
             res.status(403).send("The user doesn't have Permissions to add a match Event")
         }
         else {
             await DButils.execQuery(
-                `INSERT INTO dbo.EventDetails (MatchId, Date, Hour, TimeMinuteInGame, EventInGame, PlayerID, Description) VALUES ('${req.body.gameID}', '${req.body.Date}','${req.body.Hour}', '${req.body.TimeMinuteInGame}','${req.body.EventInGame}','${req.body.PlayerID}','${req.body.Description}')`
+                `INSERT INTO dbo.EventDetails (MatchId, Date, Hour, TimeMinuteInGame, EventInGame, PlayerID, Description) VALUES ('${req.body.MatchId}', '${req.body.Date}','${req.body.Hour}', '${req.body.TimeMinuteInGame}','${req.body.EventInGame}','${req.body.player_id}','${req.body.Description}')`
             );
             res.status(201).send("event has been added succesfully");
         }
