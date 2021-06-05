0b25a823d0104fdb0cab218965c6015306afbcf7
dvirainisch <63093025+dvirainisch@users.noreply.github.com>
2021-06-05 11:25:47 +0300

    Update league.js

--- project/routes/league.js
+++ project/routes/league.js
@@ -10,7 +10,7 @@ router.get("/getDetails", async (req, res, next) => {
     ret_arr = []
     const league_details = await league_utils.getLeagueDetails();
     ret_arr.push(league_details);
-      if(Object.entries(req.session).length != 0){ // noo goo!!!!!!!!!!!!!!!!!!!!!!!!!
+    if (req.session && req.session.user_id) {
       const match_ids = await users_utils.getFavorites("FavoriteMatches", req.session.user_id, "match_id");
       let match_ids_array = [];
       match_ids.map((element) => match_ids_array.push(element.match_id)); //extracting the matchs ids into array
